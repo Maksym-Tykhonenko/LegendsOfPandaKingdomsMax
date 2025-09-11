797c496274da09b83196925411568f0588046b52
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2025-09-11 17:47:43 +0300

    complite app

--- StackNavigator.js
+++ StackNavigator.js
@@ -580,7 +580,7 @@ const AppNavigator = () => {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2025-09-10T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2025-09-15T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (!route) {
